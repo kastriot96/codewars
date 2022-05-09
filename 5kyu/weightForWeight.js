4c5ff725517104afa9f95240dfe33aa8e11b5517
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2022-05-09 23:27:41 +0200

    final solution

--- 5kyu/weightForWeight.js
+++ 5kyu/weightForWeight.js
@@ -25,9 +25,27 @@ For C: The result is freed.
 
 */
 function orderWeight(strng) {
+  return strng
+    .split(" ")
+    .map((number) => {
+      const weight = number
+        .split("")
+        .reduce((a, b) => Number(a) + Number(b), 0);
+      return { num: number, weight };
+    })
+    .sort((a, b) =>
+      a.weight === b.weight ? a.num.localeCompare(b.num) : a.weight - b.weight
+    )
+    .map(({ num }) => num)
+    .join(" ");
+}
+
+/*
+function orderWeight(strng) {
   let sum = 0;
   for (let i = 0; i < strng.length; i++) {
     sum += parseInt(strng[i]);
   }
   return sum;
 }
+*/
