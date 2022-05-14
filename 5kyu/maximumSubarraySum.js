c0a43f2fb75b555ffd510e643065af29576720c1
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2022-05-14 20:15:23 +0200

    solution

--- 5kyu/maximumSubarraySum.js
+++ 5kyu/maximumSubarraySum.js
@@ -8,5 +8,35 @@ Easy case is when the list is made up of only positive numbers and the maximum s
 Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 */
 var maxSequence = function (arr) {
-  // ...
+  //if the array is made up only of negative numbers, return 0
+  if (
+    arr.every(function (item) {
+      return item < 0;
+    })
+  ) {
+    return 0;
+  }
+  //if the array is made up only of positive numbers, return the sum of the whole array
+  if (
+    arr.every(function (item) {
+      return item > 0;
+    })
+  ) {
+    return arr.reduce(function (a, b) {
+      return a + b;
+    });
+  }
+  //if the array is made up of both positive and negative numbers, return the sum of highest consecutive positive numbers
+  let sum = 0;
+  let maxSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    if (sum > maxSum) {
+      maxSum = sum;
+    }
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return maxSum;
 };
