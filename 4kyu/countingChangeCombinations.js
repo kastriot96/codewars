f798583099f0b0d4c056a0a0247594aa29b2e139
kastriot96 <79404857+kastriot96@users.noreply.github.com>
2022-06-13 23:17:36 +0200

    update

--- 4kyu/countingChangeCombinations.js
+++ 4kyu/countingChangeCombinations.js
@@ -39,3 +39,19 @@ function countChange(money, coins) {
   }
 }
 countChange(4);
+
+/*
+
+const countChange = function (money, coins, i = 0) {
+    if (money < 0) return 0;
+    if (money === 0) return 1;
+
+    if (i === coins.length && money > 0) {
+        return 0;
+    }
+
+    return countChange(money - coins[i], coins, i) + countChange(money, coins, i + 1);
+}
+
+*/
+// https://www.codewars.com/kata/52f677797c461daaf7000740/train/javascript
